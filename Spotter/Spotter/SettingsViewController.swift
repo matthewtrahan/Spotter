14728416f2079b4a46f29fa68215fc44d7ace4ba
matthewtrahan <mtrahan920@sbcglobal.net>
2017-04-08 14:27:35 -0500

    working on settings

--- Spotter/Spotter/SettingsViewController.swift
+++ Spotter/Spotter/SettingsViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import CoreData
 
-class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     let settings: [String] = ["Goal", "Weight"]
     var alertController: UIAlertController? = nil
     var updateField: UITextField? = nil
+    
+    // picker view for choosing a new goal
+    var goalPicker: UIPickerView = UIPickerView()
+    var goalPickerData: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +27,9 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         self.tableView.dataSource = self
         self.tableView.delegate = self
         self.tableView.tableFooterView = UIView(frame: .zero)
+        
+        // setup picker views
+        setupGoalPicker()
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,6 +58,10 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func getInfoFromCD(setting: String) -> String {
@@ -84,6 +95,11 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         return "error"
     }
     
+    // update the core data if the user selects a new goal
+    func updateCD() {
+        
+    }
+    
     func formatGoal(goal: String) -> String {
         if goal == "buildMuscle" {
             return "Build Muscle"
@@ -92,6 +108,52 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         } else {
             return "Lose Weight"
         }
+    }
+    
+    func setupGoalPicker() {
+        // Connect data
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        goalPickerData = ["Build Muscle", "Build Strength/Foundation", "Lose Weight"]
+        
+        // Creates the toolbar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        toolBar.sizeToFit()
+        
+        // Adds the buttons
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(EvenMoreAboutYouViewController.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(EvenMoreAboutYouViewController.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+    }
+    
+    // set up the done button in the toolbar to set the text field to selected data
+    func doneClick() {
+        
+    }
+    
+    // set up the cancel button to resign the picker view
+    func cancelClick() {
+
+    }
+    
+    // reload the table view to show the new data
+    func pickerView(heightPicker: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.tableView.reloadData()
+    }
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return goalPickerData.count
     }
     
     /*
